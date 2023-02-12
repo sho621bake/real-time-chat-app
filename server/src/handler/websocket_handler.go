35ebd039b266ac3f52fcf4b10f6d3d8ba74cf63a
sho621bake <sho6217@gmail.com>
2023-02-12 22:06:02 +0900

    [bugfix] websocket_handler.go

--- server/src/handler/websocket_handler.go
+++ server/src/handler/websocket_handler.go
@@ -5,24 +5,22 @@ import (
 	"net/http"
 
 	"github.com/gorilla/websocket"
-    "github.com/sho621bake/real-time-chat-app/src/domain"
-
 )
 
 type WebsocketHandler struct {}
 
 func NewWebsocketHandler() *WebsocketHandler {
-    return &WebsocketHandler{}
+	return &WebsocketHandler{}
 }
 
 func (h *WebsocketHandler) Handle(w http.ResponseWriter, r *http.Request) {
-    upgrader := &websocket.Upgrader{
-        CheckOrigin: func(r *http.Request) bool {
-            return true
-        },
-    }
-        _, err := upgrader.Upgrade(w, r, nil)
-        if err != nil {
-            log.Fatal(err)
-        }
+	upgrader := &websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+	_, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
